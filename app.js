a519a3ec6e97c6e8348e0929894804c0706a6298
Manan12 <manan@mokko.io>
2017-09-05 09:24:08 +0530

    Added the live weather forecast from an API.

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ weatherApp.config(function($routeProvider){
 			templateUrl: 'pages/forecast.htm',
 			controller: 'forecastController'
 		})
+		
+		.when('/forecast/:days', {
+			templateUrl: 'pages/forecast.htm',
+			controller: 'forecastController'
+		})
 });
 
 // SERVICES
@@ -25,20 +30,38 @@ weatherApp.service('cityService', function(){
 
 
 // CONTROLLERS
-weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('homeController', ['$scope',  'cityService', function($scope, cityService) {
 	
 		$scope.city = cityService.city;
 		
 		$scope.$watch('city', function(){
 			cityService.city = $scope.city;
-		}
-			)
+		})
+		
+		
+			
     
 }]);
 
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+weatherApp.controller('forecastController', ['$scope', 'cityService', '$resource', '$routeParams', function($scope, cityService, $resource, $routeParams) {
 		
 		$scope.city = cityService.city;
+		
+		$scope.days = $routeParams.days || 2;
+		
+		$scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=f429911348202563cf2e9d6c2b18058b");
+		
+		$scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days});
+		
+		$scope.convertToFarenheit = function(degK){
+			return Math.round((1.8 * (degK - 273)) + 32);
+		}
+		
+		$scope.convertToDate = function(dt){
+			return new Date(dt * 1000);
+		}
+		
+		
     
 }]);
